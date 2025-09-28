3c29870fded6fb228167e70e248e096b92b39c0b
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-09-28 16:52:56 +0545

    feat: add logo feature

---
+++ frontend/src/features/logo/Logo.tsx
@@ -0,0 +1,11 @@
+
+
+function Logo () {
+    return (
+        <div className="absolute top-4 left-4 p-4">
+            <img src="/logo.svg" alt="logo icon" />
+        </div>
+    )
+}
+
+export default Logo;
