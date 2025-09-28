58dfd33c49587d9f9677469f00a3d3074ebf18d3
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-09-28 16:50:30 +0545

    feat: add logo in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import BackgroundWrapper from './components/background/BackgroundWrapper'
+import Logo from './features/logo/Logo'
+import machapuchhre from './assets/themes/machhapuchhre.jpg'
 
 function App() {
+  const [backgroundImgUrl, setBackgroundImgUrl] = useState(machapuchhre);
 
   return (
     <>
-      <h1 className="text-gray-600">Pomodoro</h1>
+      <BackgroundWrapper imgUrl={backgroundImgUrl}>
+        <div className="relative h-full w-full">
+          <Logo />
+        </div>
+      </BackgroundWrapper>
     </>
   )
 }
